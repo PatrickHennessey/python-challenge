3198326f67732d32647ade38193b07e86b869270
PatrickHennessey <phennessey@ucsd.edu>
2019-08-13 16:00:20 -0700

    Added later versions of the main.py files and outputs.

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,65 +1,57 @@
 import os
 import csv
 
-# input path
+# Opens the csv file with polling data
 py_bank_csv = os.path.join('Resources', 'budget_data.csv')
 
-# output path
+# In preparation for printing out the results text file.
 py_bank_output = os.path.join('PyBank_Results.txt')
 
-# Create the necessary lists and variables for the calculations.
+## Variable Declarations ##
+# Create the necessary empty lists used for calculations.
 total_months = []
 total_revenue = []
 average_change = []
 
-# Open csv file, set the reader and skip the header row as usual
+# Open csv file, set the reader and skips the header row as usual
 with open(py_bank_csv, 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     header = next(csvreader)
 
-    # loops through the rows in the file and add to the lists defined above
+    # Loops through the rows in the file and add to the lists defined above ...
     for row in csvreader:
-        # Gives a total based on the number of rows in column 1
+        # ... Gives a total based on the number of rows in column 1 ...
         total_months.append(row[0])
-        # Adds the integer value of each or the rows in colmn 2 together to give a result
+        # ... Adds the integer value of each or the rows in colmn 2 together to give a result.
         total_revenue.append(int(row[1]))
 
-    # loops through the Profits/Losses row to calculate the monthly profit change
-    # the -1 removes the blank entry from the calculation, since we need to find the result between two entries
+    # Loops through the Profits/Losses row to calculate the monthly profit change
+    # The "range(len(total_revenue) - 1):" removes the blank entry from the calculation, since we need to find the result between two entries
     for i in range(len(total_revenue) - 1):
         average_change.append(total_revenue[i + 1] - total_revenue[i])
 
 # Grab the biggest profit and loss total
 max_increase_change = max(average_change)
-max_decrease_chane = min(average_change)
+max_decrease_change = min(average_change)
 
 # Grab the corresponding months for profit and loss by looking ahead +1 from current posistion
 max_increase_month = average_change.index(max(average_change)) + 1
 max_decrease_month = average_change.index(min(average_change)) + 1
 
-# Print totals to the terminal using the \n command to forgoe using multiple "print()" statements
-print(
-    f"Financial Analysis \n"
-    f"------------------------- \n"
-    f"Total Months: {len(total_months)} months \n"
-    f"Total Profits: ${sum(total_revenue)} \n"
-    f"Average Change: ${round(sum(average_change) / len(average_change), 2)} \n"
-    f"Greatest Increase in Profits: {total_months[max_increase_month]} ${max_increase_change} \n"
-    f"Greatest Decrease in Profits: {total_months[max_decrease_month]} ${max_decrease_chane} \n"
-)
-
 # Print results to a file named "PyBank_Results.txt"
 with open(py_bank_output, "w") as file:
-    file.write("Financial Analysis")
-    file.write(f"\n")
-    file.write(f"-------------------------")
-    file.write(f"\n")
-    file.write(f"Total Months: {len(total_months)} months")
-    file.write(f"\n")
-    file.write(f"Total Profits: ${sum(total_revenue)}")
-    file.write(f"\n")
-    file.write(f"Average Change: ${round(sum(average_change) / len(average_change), 2)}")
-    file.write(f"\n")
-    file.write(f"Greatest Increase in Profits: {total_months[max_increase_month]} ${max_increase_change}")
-    file.write(f"\n")
-    file.write(f"Greatest Decrease in Profits: {total_months[max_decrease_month]} ${max_decrease_chane}")
+    # ".writelines" allows you to print to multiple lines without haveing to do multiple file.writes statements.
+    # The "\n" is like a carriage return. You could shorten everything by getting rid of the multple f"" lines,
+    # but then you have one long line that is diffficult to read without scrolling    file.writelines(
+        file.writelines(f"Financial Analysis\n"
+        f"---------------------------\n"
+        f"Total Months: {len(total_months)} months\n"
+        # ":.2f" formats the results to have two decimal places.
+        f"Total Profits: ${sum(total_revenue):.2f}\n"
+        f"Average Change: ${round(sum(average_change) / len(average_change), 2)}\n"
+        f"Greatest Increase in Profits: {total_months[max_increase_month]} {max_increase_change:.2f}\n"
+        f"Greatest Profit Loss: {total_months[max_decrease_month]} ${max_decrease_change:.2f}\n")
+    
+## Prints results to the terminal
+with open(py_bank_output, 'r') as reader:
+    print(reader.read())
---
+++ PyPoll/main.py
@@ -0,0 +1,76 @@
+import os
+import csv
+
+# Opens the csv file with polling data
+py_poll_csv = os.path.join('Resources', 'election_data.csv')
+
+# In preparation for printing out the results text file.
+py_poll_output = os.path.join('PyPoll_restults.txt')
+
+## Variable Declarations ##
+# Inits the total vote counter.
+total_votes = 0
+# creates an empty Dictionary to hold candidate names and their votes.
+poll_results = {}
+# Create the necessary empty lists used for tracking and calculations.
+candidates = []
+votes = []
+percent_of_votes = []
+who_is_winner = []
+
+# Opens the csv file with polling data and skips the header row.
+with open(py_poll_csv, 'r') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    header = next(csvreader)
+
+    # Loop through the rows ...     
+    for row in csvreader:
+        # ... adding to the total_vote counter ...
+        total_votes += 1
+        # ... Looks for unique values in the 'Candidate' row,
+        # ".keys()" will add a unique name to the dictionary (only once since you 
+        # can't have two different keys with the same name) ...
+        if row[2] in poll_results.keys():
+            # ... and adds to their vote count.
+            poll_results[row[2]] = poll_results[row[2]] + 1
+        else:
+            poll_results[row[2]] = 1
+
+# Takes the information in the Dictionary, puts each set into the declared lists, and makes candidate the key and votes the value
+for i, j in poll_results.items():
+    candidates.append(i)
+    votes.append(j)
+
+# Loops through the above list of votes and get the percentage for each candidate.
+for i in votes:
+    percent_of_votes.append(round(i / total_votes * 100, 1))
+
+# Zip the lists (candidates, votes, and percent_of_votes together) into a new list
+voting_results = list(zip(candidates, votes, percent_of_votes))
+
+# Looks through the voteing_results list to find the candidate with the .max votes and adds their name to the list.
+for candidate in voting_results:
+    if max(votes) == candidate[1]:
+        who_is_winner.append(candidate[0])
+
+# winner = who_is_winner
+
+# Print results to a file named "PyPol_Results.txt"
+with open(py_poll_output, "w") as file:
+    # ".writelines" allows you to print to multiple lines without haveing to do multiple file.writes statements.
+    # The "\n" is like a carriage return. You could shorten everything by getting rid of the multple f"" lines, 
+    # but then you have one long line that is diffficult to read without scrolling
+    file.writelines(f"---------------------------\n"
+    f"Election Results\n"
+    f"---------------------------\n"
+    f"Total Votes: {(total_votes)}\n")
+    for candidate in voting_results:
+        # ":.3f" formats the results to have three decimal places.
+        file.writelines(f"{str(candidate[0])}: {int(candidate[2]):.3f}%  ({int(candidate[1])})\n")
+    file.writelines(f"------------------------- \n"
+                    f"Winner: {str(who_is_winner[0])}\n"
+                    f"-------------------------")
+        
+## Prints results to the terminal 
+with open(py_poll_output, 'r') as reader:
+    print(reader.read())
